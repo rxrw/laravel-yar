e13c65ca61c0a9f177ead35d04153d8ca5277d15
Jens Lee <rxrw@me.com>
2018-08-15 11:15:36 +0800

    加了些有的没的注释

--- src/Yar.php
+++ src/Yar.php
@@ -7,23 +7,30 @@
  */
 
 namespace Reprover\LaravelYar;
+
 use Yar_Client;
 
 class Yar
 {
 
-    public static function call($mapName, $parameters){
-        $mapConfig = config("yar-map.".$mapName);
+    public static function call($mapName, $parameters)
+    {
+        $mapConfig = config("yar-map." . $mapName);
         $module = $mapConfig['module'];
-        $serviceConfig = config("yar-services.".$module);
+        $serviceConfig = config("yar-services." . $module);
         $url = $serviceConfig['path'] . $serviceConfig['services'][$mapConfig['service']];
         $yarClient = new Yar_Client($url);
-        if(isset($mapConfig['connect_timeout']))$yarClient->setOpt(YAR_OPT_CONNECT_TIMEOUT,$mapConfig['connect_timeout']);
-        if(isset($mapConfig['read_timeout']))$yarClient->setOpt(YAR_OPT_TIMEOUT,$mapConfig['connect_timeout']);
+        if (isset($mapConfig['connect_timeout'])) {
+            $yarClient->setOpt(YAR_OPT_CONNECT_TIMEOUT, $mapConfig['connect_timeout']);
+        }
 
-        try{
+        if (isset($mapConfig['read_timeout'])) {
+            $yarClient->setOpt(YAR_OPT_TIMEOUT, $mapConfig['connect_timeout']);
+        }
+
+        try {
             $res = $yarClient->{$mapConfig['method']}($parameters);
-        }catch (Yar_Client_Exception $e){
+        } catch (Yar_Client_Exception $e) {
             throw $e;
         }
         return $res;
